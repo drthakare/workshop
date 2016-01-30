e8a0d2a9eac85857d3b9e5729b6f0761ffd169fb
drthakare <drthakare@email.arizna.edu>
2016-01-30 16:22:08 -0700

    initial commit

---
+++ iteration.py
@@ -0,0 +1,4 @@
+books  = [" tales of two" , "Herry Pott", "Lord of the Rings"]
+nums = range(100)
+for book in books:
+    print (b)
---
+++ iteration1.py
@@ -0,0 +1,4 @@
+books  = [" tales of two" , "Harry Pott", "Lord of the Rings"]
+nums = range(100)
+for b in books:
+    print (b)
